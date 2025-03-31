fb658bb55c70c99bdf9a2435a95c09eef192df98
Ori Adijes <ori@wnwd.com>
2025-03-31 23:13:39 +0300

    foreign key referencing

--- orders-service/src/main/java/com/example/orders/controllers/MainController.java
+++ orders-service/src/main/java/com/example/orders/controllers/MainController.java
@@ -1,7 +1,9 @@
 package com.example.orders.controllers;
 
 import com.example.orders.entities.Order;
+import com.example.orders.entities.User;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -13,4 +15,11 @@ public class MainController {
     public List<Order> orders() {
         return Order.getAll();
     }
+
+    @SchemaMapping
+    public User user(Order order) {
+        return new User(order.userId());
+    }
 }
+
+
--- users-service/src/main/java/com/example/users/controllers/MainController.java
+++ users-service/src/main/java/com/example/users/controllers/MainController.java
@@ -1,6 +1,8 @@
 package com.example.users.controllers;
 
 import com.example.users.entities.User;
+import org.springframework.graphql.data.federation.EntityMapping;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -8,6 +10,11 @@ import java.util.List;
 
 @Controller
 public class MainController {
+
+    @EntityMapping
+    public List<User> user(@Argument List<String> idList) {
+        return idList.stream().map(User::getById).toList();
+    }
 
     @QueryMapping
     public List<User> users() {
