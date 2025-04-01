66d9fb54fcb87016d8c9ec0fc226eca1a3f71258
Ori Adijes <ori@wnwd.com>
2025-04-01 18:11:34 +0300

    Added more logs

--- users-service/src/main/java/com/example/users/controllers/MainController.java
+++ users-service/src/main/java/com/example/users/controllers/MainController.java
@@ -1,6 +1,8 @@
 package com.example.users.controllers;
 
 import com.example.users.entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.graphql.data.federation.EntityMapping;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
@@ -10,9 +12,13 @@ import java.util.List;
 
 @Controller
 public class MainController {
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
 
     @EntityMapping
     public List<User> user(@Argument List<String> idList) {
+        logger.info("@EntityMapping (User) called with idList = {}", idList);
+
         return idList.stream().map(User::getById).toList();
     }
 
