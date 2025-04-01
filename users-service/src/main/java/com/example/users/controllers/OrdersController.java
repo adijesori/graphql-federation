2c17df3bfa7ae5716b379f8d3dab743a6294689f
Ori Adijes <ori@wnwd.com>
2025-04-01 18:08:24 +0300

    Added more logs

--- users-service/src/main/java/com/example/users/controllers/OrdersController.java
+++ users-service/src/main/java/com/example/users/controllers/OrdersController.java
@@ -2,6 +2,8 @@ package com.example.users.controllers;
 
 import com.example.users.entities.Order;
 import com.example.users.entities.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.graphql.data.federation.EntityMapping;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
@@ -9,8 +11,11 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class OrdersController {
+    private static final Logger logger = LoggerFactory.getLogger(OrdersController.class);
+
     @EntityMapping
     public Order order(@Argument String id, @Argument String userId) {
+        logger.info("@EntityMapping (Order) called with orderId = {} and userId = {}", id, userId);
         return new Order(id, userId);
     }
 
